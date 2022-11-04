1dcd69921c9ba73f0979ecbc262276bf55e327ad
Marcelosky <110744970+Marcelosky@users.noreply.github.com>
2022-11-04 22:49:20 +0100

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,4 @@
+nom = prompt("Cual es tu nombre?")
+alert("Binevenido " + nom + " a mi primera pagina web")
+alert("Trabajo del modulo 1 de aplicaciones web")
+alert("se utiliza efectos con hover  ")
